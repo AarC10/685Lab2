e9fe2599e0a02173acb691a7cc76f6c4e76d6b6e
Aaron Chan <aarchan108@gmail.com>
2025-09-19 15:15:34 -0400

    Repeatability test section

---
+++ src/lynxmotion_ssc32/src/repeatability_test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import JointState
+
+joint_publisher = rospy.Publisher('/joint_states', JointState, queue_size=10)
+joint = JointState()
+joint.name = ["base", "shoulder", "elbow", "wrist", "wrist_twist", "gripper"]
+joint.position = [0, 0, 0, 0, 0, 0]
+THETA_INCREMENT = 0.01
+reverse = False
+counter = 0
+
+TARGET_SHOULDER = -0.565
+TARGET_ELBOW = 0.141
+TARGET_WRIST = 0.353
+
+SHOULDER_INCREMENT = TARGET_SHOULDER / 100
+ELBOW_INCREMENT = TARGET_ELBOW / 100
+WRIST_INCREMENT = TARGET_WRIST / 100
+
+def move0():
+    global joint
+    global reverse
+    global counter
+    increment = THETA_INCREMENT
+    if reverse:
+        increment *= -1
+
+    joint.position[1] += SHOULDER_INCREMENT # shoulder
+    joint.position[2] += ELBOW_INCREMENT # elbow
+    joint.position[3] += WRIST_INCREMENT # wrist
+
+
+    joint.header.stamp = rospy.Time.now()
+
+    counter += 1
+    if counter > 100:
+        counter = 0
+        reverse = not reverse
+
+    joint_publisher.publish(joint)
+    rospy.sleep(.05)
+
+
+if __name__ == '__main__':
+    rospy.init_node('Movement')
+    try:
+        joint.position[0] = 0
+        joint.position[1] = 0
+        joint.position[2] = 0
+        joint.position[3] = 0
+        joint.position[4] = 0
+        joint.position[5] = 1.411 # Gripper for pen
+
+
+        while not rospy.is_shutdown():
+            move0()
+            
+    except rospy.ROSInterruptException:
+        pass
